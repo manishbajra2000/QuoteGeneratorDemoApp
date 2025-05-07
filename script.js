8a7e309854c8d64dc723c05aca6cb00b3bffa094
Manish Harsha Bajracharya <mantu.bajra2000@gmail.com>
2025-05-07 14:02:31 +0545

    Added script.js file with few quotes and category

---
+++ script.js
@@ -0,0 +1,10 @@
+const quotes = [
+    {text: "This is a motivational quote", category: "inspirational"},
+    {text: "This is a inspirational quote", category: "inspirational"},
+    {text: "This is a fitness quote", category: "fitness"},
+    {text: "This is a workout quote", category: "fitness"},
+    {text: "This is a scientific quote", category: "science"},
+    {text: "This is a sciencey quote", category: "science"}
+  ];
+
+  

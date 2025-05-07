00f9f0d7cd8985fdb1ddb6311f29cacb7b873767
Manish Harsha Bajracharya <mantu.bajra2000@gmail.com>
2025-05-07 15:19:25 +0545

    Added displayQuote() and EventListener when changing categories.

--- script.js
+++ script.js
@@ -1,10 +1,51 @@
 const quotes = [
-    {text: "This is a motivational quote", category: "inspirational"},
-    {text: "This is a inspirational quote", category: "inspirational"},
-    {text: "This is a fitness quote", category: "fitness"},
-    {text: "This is a workout quote", category: "fitness"},
-    {text: "This is a scientific quote", category: "science"},
-    {text: "This is a sciencey quote", category: "science"}
-  ];
+    {text: 'This is a inspirational quote', category: 'inspiration'},
+    {text: 'This is a fitness quote', category: 'fitness'},
+    {text: 'This is a motivational quote', category: 'inspiration'},
+    {text: 'This is a workout quote', category: 'fitness'},
+    {text: 'This is a scientific quote', category: 'science'},
+    {text: 'This is a sciencey quote', category: 'science'}
+];
+
+let currentIndex = 0;
+let currentCategory = 'all';
+
+const categorySelect = document.getElementById('category-select');
+const quoteText = document.getElementById('quote-text');
+
+// const getFilteredQuotes = () => {
+//     if (currentCategory === 'all') {
+//         return quotes;
+//     }
+//     else {
+//         return quotes.filter(q => q.category === currentCategory);
+//     }
+// }
+//
+// which is same as using ternary operator one-code below:
+
+const getFilteredQuotes = () => currentCategory === 'all' ? quotes : quotes.filter(q => q.category === currentCategory);
+
+const displayQuote = () => {
+    const filteredQuotes = getFilteredQuotes();
+    if (filteredQuotes.length === 0) {
+        quoteText.textContent = 'No quotes available for this category.';
+        return;
+    }
+    currentIndex = (currentIndex + filteredQuotes.length) % filteredQuotes.length;
+    quoteText.textContent = filteredQuotes[currentIndex].text;
+}
+
+categorySelect.addEventListener('change', (e) => {
+    currentCategory = e.target.value;
+    currentIndex = 0;
+    displayQuote();
+});
+
+displayQuote();
+
+
+
+
 
   
